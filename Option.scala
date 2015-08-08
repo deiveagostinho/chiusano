805e856fc287003d7ea2719e2cc14191f986e5a5
Ju Gonçalves <junia.goncalves@gmail.com>
2015-08-08 13:27:56 +0200

    add almost all exercises on Option

--- Option.scala
+++ Option.scala
@@ -1,15 +1,35 @@
 import scala.{Option => _, Some => _, Either => _, _}
 
 sealed trait Option[+A]{
-  def map[B](f: A => B): Option[B] = ???
+  def map[B](f: A => B): Option[B] =
+    this match {
+      case None =>  None
+      case Some(a) => Some(f(a))
+    }
 
-  def getOrElse[B>:A](default: => B): B = ???
+  def getOrElse[B>:A](default: => B): B =
+    this match {
+      case None => default
+      case Some(a) => a
+    }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    this match {
+      case None => None
+      case Some(a) => f(a)
+    }
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
+  def orElse[B>:A](ob: => Option[B]): Option[B] =
+    this match {
+      case None => ob
+      case Some(a) => Some(a)
+    }
 
-  def filter(f: A => Boolean): Option[A] = ???
+  def filter(f: A => Boolean): Option[A] =
+    this match {
+      case None => None
+      case Some(a) => if (f(a)) Some(a) else None
+    }
 }
 
 case class Some[+A](get: A) extends Option[A]
@@ -37,8 +57,30 @@ object Option {
     if(xs.isEmpty) None
     else Some(xs.sum / xs.length)
 
-  def variance(xs: Seq[Double]): Option[Double] = ???
-  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  def variance(xs: Seq[Double]): Option[Double] =
+    this.flatMap(mean)
+
+  def lift[A, B](f: A => B): Option[A] => Option[B] = _ map f
+
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    (a, b) match {
+      case (None, _) => None
+      case (_, None) => None
+      case (Some(a), Some(b)) => Some(f(a, b))
+    }
+
+  def map2_2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option =
+    a flatMap(aa => b map (bb => f(aa, bb)))
+
+  def map2_3[A, B, C](a: Option[A], b: Option[b])(f: (A, B) => C): Option =
+    for {
+      aa <- a
+      bb <- b
+    } yield f(aa, bb)
+
+  def bothMatch(pat1: String, pat2: String, s: String): Option[Boolean] =
+    map2(mkMatcher(pat1), mkMatcher(pat2), (f, g) => f(s) && g(s))
+
   def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
   def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = ???
 }
